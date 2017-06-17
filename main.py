a4162624a89afb7ea845a69ae582c5ec63fbb1be
doebi <christoph@doeberl.at>
2017-06-17 18:07:48 +0200

    first working version

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import network, badge, ugfx, json
+from time import *
 import urequests as requests
 
-apikey = "e502c2eb253e6a6712cb0a06906832d6"
+apikey = ""
 
 sta_if = network.WLAN(network.STA_IF);
 sta_if.active(True)
@@ -11,6 +12,30 @@ sta_if.isconnected()
 badge.init()
 ugfx.init()
 
+icons = {
+"clear-day": "\uf00d",
+"clear-night": "\uf02e",
+"rain": "\uf019",
+"snow": "\uf01b",
+"sleet": "\uf0b5",
+"wind": "\uf050",
+"fog": "\uf021",
+"cloudy": "\uf041",
+"partly-cloudy-day": "\uf002",
+"partly-cloudy-night": "\uf086"
+}
+
+def getDisplayName(time):
+    #if time[3] == 0:
+    #    n = "Night"
+    #if time[3] == 6:
+    #    n = "Morning"
+    #if time[3] == 12:
+    #    n = "Noon"
+    #if time[3] == 18:
+    #    n = "Evening"
+    #return str(time[2]) + "." + str(time[1]) + ". " + n
+    return str(time[2]) + "." + str(time[1]) + ". " + str(time[3]) + ":00"
 
 # loading screen
 ugfx.clear();
@@ -18,32 +43,58 @@ ugfx.string(10,10,"calling the stars...","Roboto_Regular12", 0)
 ugfx.flush()
 
 # fetching forecast data
-url = "https://api.darksky.net/forecast/"+apikey+"/37.8267,-122.4233?units=ca"
+url = "https://api.darksky.net/forecast/"+apikey+"/52.3451,5.4581?units=ca"
 r = requests.get(url)
 data = r.json()
 r.close()
 
+# aligning according to time
+hourly = data["hourly"]["data"]
+starttime = localtime(hourly[0]["time"])
+if starttime[3] > 6:
+    index = 12 - starttime[3]
+
+if starttime[3] > 12:
+    index = 18 - starttime[3]
+
+if starttime[3] > 18:
+    index = 24 - starttime[3]
+
+timepos = (10, 30)
+iconpos = (10, 44)
+temppos = (0, 100)
+
 # displaying forecast
 ugfx.clear();
-ugfx.string(5, 5, "Vienna, 04.08.2017", "Roboto_Regular12", 0)
+ugfx.line(0, 26, 296, 26, 0)
+ugfx.line(0, 44, 296, 44, 0)
+ugfx.string(5, 5, "Weather Forecast for: Zeewolde, NL", "pixelade13", 0)
 
-ugfx.string(9, 20, "MORNING", "Roboto_Regular12", 0)
-ugfx.string(92, 20, "NOON", "Roboto_Regular12", 0)
-ugfx.string(158, 20, "EVENING", "Roboto_Regular12", 0)
-ugfx.string(240, 20, "NIGHT", "Roboto_Regular12", 0)
+dp = hourly[index]
+time = localtime(dp["time"])
+ugfx.string(timepos[0], timepos[1], getDisplayName(time), "pixelade13", 0)
+ugfx.string(iconpos[0], iconpos[1], icons[dp["icon"]], "weather42", 0)
+ugfx.string(temppos[0], temppos[1], str(int(dp["temperature"]))+"°C", "DejaVuSansMono20", 0)
 
-#xi = 2
-#for point in data["hourly"]["data"]:
-#    ugfx.area(xi, 0, 5, int(point["temperature"]) * 3, 0)
-#    xi += 6
+dp = hourly[index+6]
+time = localtime(dp["time"])
+ugfx.string(timepos[0] + 1*11 + 1*64, timepos[1], getDisplayName(time), "pixelade13", 0)
+ugfx.string(iconpos[0] + 1*11 + 1*64, iconpos[1], icons[dp["icon"]], "weather42", 0)
+ugfx.string(temppos[0] + 1*11 + 1*64, temppos[1], str(int(dp["temperature"]))+"°C", "DejaVuSansMono20", 0)
 
-#for point in data["hourly"]["data"]:
-#    print(point["icon"])
+dp = hourly[index+12]
+time = localtime(dp["time"])
+ugfx.string(timepos[0] + 2*11 + 2*64, timepos[1], getDisplayName(time), "pixelade13", 0)
+ugfx.string(iconpos[0] + 2*11 + 2*64, iconpos[1], icons[dp["icon"]], "weather42", 0)
+ugfx.string(temppos[0] + 2*11 + 2*64, temppos[1], str(int(dp["temperature"]))+"°C", "DejaVuSansMono20", 0)
 
-ugfx.box(5, 59, 64, 64, 0)
-ugfx.box(5 + 1*10 + 1* 64, 59, 64, 64, 0)
-ugfx.box(5 + 2*10 + 2* 64, 59, 64, 64, 0)
-ugfx.box(5 + 3*10 + 3* 64, 59, 64, 64, 0)
-
+dp = hourly[index+18]
+time = localtime(dp["time"])
+ugfx.string(timepos[0] + 3*11 + 3*64, timepos[1], getDisplayName(time), "pixelade13", 0)
+ugfx.string(iconpos[0] + 3*11 + 3*64, iconpos[1], icons[dp["icon"]], "weather42", 0)
+ugfx.string(temppos[0] + 3*11 + 3*64, temppos[1], str(int(dp["temperature"]))+"°C", "DejaVuSansMono20", 0)
 
 ugfx.flush();
+
+while True:
+    pass
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 # A Weatherapp for the SHA2017 Badge
+
+## Icons
+clear-day: f00d
+clear-night: f02e
+rain: f019
+snow: f01b
+sleet: f0b5
+wind: f050
+fog: f021
+cloudy: f041
+partly-cloudy-day: f002
+partly-cloudy-night: f086
